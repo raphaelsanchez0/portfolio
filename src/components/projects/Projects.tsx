81e23a9eea345c32ff16ff49a27bfdefbe46f563
Raphael Sanchez <40778747+raphaelsanchez0@users.noreply.github.com>
2024-08-09 17:44:51 -0400

    feat: change skills to projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Card, CardHeader, CardTitle } from "../ui/card";
 
-export default function Skills() {
+export default function Projects() {
   return (
     <div className="w-full py-6">
-      <h2 className="lg:text-3xl font-semibold text-center py-2">Skills</h2>
+      <h2 className="lg:text-3xl font-semibold text-center py-2">Projects</h2>
       <div className="flex flex-col">
         <Card>
           <CardHeader>
